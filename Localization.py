aafde74cb3bd9f251c5306b0c712cbaa51e62a57
Przemysław Kowalewski <irqize@gmail.com>
2020-11-29 14:07:18 +0000

    pierdu pierdu

--- Localization.py
+++ Localization.py
@@ -20,3 +20,36 @@ def plate_detection(image):
     #Replace the below lines with your code.
     plate_imgs = image
     return plate_imgs
+
+cap = cv2.VideoCapture("Video7_2.avi")
+ret, frame = cap.read()
+
+import matplotlib.pyplot as plt
+
+#blur = cv2.GaussianBlur(frame, (7,7), 0)
+filter = cv2.bilateralFilter(frame, 17, 50, 150)
+gray = cv2.cvtColor(filter, cv2.COLOR_BGR2GRAY)
+
+
+edged = cv2.Canny(gray, 10, 170)
+
+contours, ne  = cv2.findContours(edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+contours = sorted(contours, key=cv2.contourArea, reverse=True)[:30]
+
+cntrs = []
+m = 0
+for c in contours:
+    peri = cv2.arcLength(c, True)
+    approx = cv2.approxPolyDP(c, 0.1 * peri, True)
+    m = max(len(approx), m)
+    if len(approx) == 4:
+        cntrs.append(c)
+        break
+
+print(m)
+
+mask = np.zeros(gray.shape, np.uint8)
+# img_cntrd = cv2.drawContours(mask, [cntrs], 0, 255, -1)
+# img_cntrd = cv2.bitwise_and(frame, frame, mask=mask)
+plt.imshow(cv2.drawContours(frame, contours, -1, (255, 0, 0), 3))
+plt.show()
